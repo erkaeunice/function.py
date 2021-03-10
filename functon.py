9e87e11696cce1536facca8e4d8a3e7d6e4d0c64
erkaeunice <77729926+erkaeunice@users.noreply.github.com>
2021-03-10 16:52:50 +0300

    Add files via upload

---
+++ functon.py
@@ -0,0 +1,75 @@
+#creating a function
+def my_function():
+  print("Hello Word")
+my_function()
+
+#argument
+def sum(k,x):
+    a=k+x
+    print("The sum is",a)
+sum(20,30)
+sum(501,845)
+
+def product(k,x):
+    a=k*x
+    print("The product is",a)
+product(20,30)
+#argument
+def course(*args):
+   for subject in args:
+      print(subject)
+course("Big Data", "CCNA", "OOP2")
+#args
+def courses(*args):
+   
+  for subject in args:
+     return subject 
+print(courses("Big Data", "CCNA", "oop2", "Computer Netwotk"))
+
+def course(**kwargs):
+  for key,value in kwargs.items():
+    print("{}: {}".format(key,value))
+courses(first='Big Data', second
+def kenya(county= "Mombasa"):
+   print("I am from "+ county)
+  
+kenya()
+kenya("Nairobi")
+kenya("Kiambu")
+kenya("Kisumu
+
+#passing a list as argument
+def my_function(food):
+   for r in food:
+     print(r)
+     
+fruits= {"orange", "pinapple", "mango"}
+my_function(fruits)
+
+#passing dictionary argument
+car ={"brand": "ford", "model" "mustarng", "year";186}
+ny_fuction(car)
+
+#area of crcle
+def findArea(s):
+  P1 =3.142
+  return P1 * (s*s);
+
+num=float(input("enter s value :"))
+print("area is %.1f" % findArea(num));
+
+     #area of rectangle
+def findArea(k,r):
+  a=k*r*r
+  return k
+  print(Area(3.142,7))
+
+  def Area(1):
+      k=1*1
+    return k
+print(Area(7))
+
+
+
+  
+
